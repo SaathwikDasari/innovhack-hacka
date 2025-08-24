1d5ff1fff698e42437ac0c8b6e0b5fb640f66b7c
Saathwik Dasari <saathwik.dasari@gmail.com>
2025-08-24 07:00:15 +0000

    logins

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def home():
     return render_template('login.html')
 
+@app.route('/stafflog')
+def stalog():
+    return render_template('staffLogin.html')
+
+@app.route('/stulogin')
+def stulog():
+    return render_template('stuLogin.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
